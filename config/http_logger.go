e9f3be480481ff1a585352e6a9fdf6234ac2a479
Dzemil Dupljak <dzemilduplak4795@gmail.com>
2023-12-23 20:48:55 +0100

    add http req/res logger t2

---
+++ config/http_logger.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type responseWriterWithStatus struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rw *responseWriterWithStatus) WriteHeader(statusCode int) {
+	rw.status = statusCode
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
+
+func LoggerMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Log information about the incoming request.
+		start := time.Now()
+		fmt.Printf("Incoming HTTP Request: %s %s\n", r.Method, r.URL.Path)
+
+		// Create a custom ResponseWriter to capture the status code.
+		rw := &responseWriterWithStatus{ResponseWriter: w}
+
+		// Call the next handler in the chain.
+		next(rw, r)
+
+		// Log information about the outgoing response.
+		duration := time.Since(start)
+		fmt.Printf("  Outgoing HTTP Response: Status %d, Duration %v\n", rw.status, duration)
+	}
+}
